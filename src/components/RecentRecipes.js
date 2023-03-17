dd01fe546b2c22008c64896f5af50050db3c38e7
Mariam-asiimwe <m.asiimwe@outlook.com>
2023-03-17 19:36:15 +0000

    added code for the recent recipes feature

---
+++ src/components/RecentRecipes.js
@@ -0,0 +1,24 @@
+
+
+// let recentRecipes = "";
+
+function RecentRecipes() {
+  return (
+
+
+    <div className="container text-center">
+      <h3 style="margin-bottom: 2rem">Recent recipes</h3>
+      <div id="recent-recipes-list-row" className="row"></div>
+
+
+
+    </div>
+  );
+
+}
+
+
+export default RecentRecipes;
+
+
+{/* let apiUrl = "https://api.edamam.com/api/recipes/v2?type=public&app_id=25373478&apiKey=d7cbf056cc18d4141af4063adcd2a63a"; */ }
