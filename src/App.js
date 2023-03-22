49bc492fe265be42022ccdb393e857855e36c2d0
Vida Nsiah <vidansiah@Vidas-MacBook-Air.local>
2023-03-22 18:32:03 +0000

    adding recipeNavebar

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Button from "react-bootstrap/Button"
 import './App.scss';
 import { useState } from 'react';
 import Footer from "./Components/Pages/Footer";
+import RecipeNavbar from "./Components/RecipeNavbar";
 
 
 function App() {
+
   const [recipes,setRecipes]=useState([])
   const [searchTerm,setSearchTerm]=useState("chicken")
   let apiUrl ="https://api.edamam.com/api/recipes/v2?type=public&app_id=0639710c&app_key=e94e7c998f0107fb2b6478641c6d762d&q="
@@ -18,16 +20,20 @@ function App() {
   function handleSubmit(){
     getRecipes(searchTerm)
   }
-  
+
   return (
+
+   
+
     <div className="App">
-      <input value={searchTerm}onChange={(event)=>setSearchTerm(event.target.value)}/>
-      <Button variant='primary' onClick={handleSubmit}>Submit</Button> 
+      {/* <input value={searchTerm}onChange={(event)=>setSearchTerm(event.target.value)}/>
+      <Button variant='primary' onClick={handleSubmit}>Submit</Button>  */}
       {recipes && recipes.map((item, index)=>{
         return <div key={index}>{item.recipe.label}
         <img src={item.recipe.image} alt=""/>
         </div>
       })}
+       <RecipeNavbar></RecipeNavbar>
       <Footer></Footer>
     </div>
   );
